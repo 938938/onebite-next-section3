af8b456a54d775bc574da2531be92240fa7e6e92
938938 <938kjh@gmail.com>
2024-10-06 23:39:11 +0900

    [6.2] 페이지 스트리밍 적용하기

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,13 +3,6 @@ import style from './page.module.css';
 import { BookData } from '@/types';
 import { API_URL } from '@/components/global';
 
-// export const dynamic = '';
-// 특정 페이지의 유형을 강제로 static, dynamic 페이지로 설정.
-// 1. auto : 기본값, 아무것도 강제하지 않음.
-// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정.
-// 3. force-static : 페이지를 강제로 Static 페이지로 설정. 쿼리스트링 등의 값은 undefined로 설정됨.(오류 발생 가능성)
-// 4. error : 페이지를 강제로 Static 페이지로 설정. (쿼리스트링 등으로 Static으로 설정하면 안되는 이유가 있다면 빌드 오류 발생)
-
 async function AllBooks() {
   const response = await fetch(`${API_URL}/book`, { cache: 'force-cache' });
   if (!response.ok) {
